ca7be66f84fb8e543deb4a61e5ac51d7081b294c
joshi232004manish <manishjoshi232004@gmail.com>
2025-01-28 05:09:00 +0530

    Added Trust and controller logic

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import cookieParser from "cookie-parser"
 import userAuth from './routes/userAuth.js';
+import userRoute from './routes/user.route.js';
+import listingRoute from './routes/listing.route.js';
 import dotenv from 'dotenv';
+import chatRoute from './routes/chat.route.js';
+import trustRoute from './routes/trust.route.js';
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 const port = 5000;
@@ -16,6 +26,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',userAuth);
+app.use('/api/users',userRoute);
+app.use('/api/listings',listingRoute);
+app.use('/api/chat', chatRoute);
+app.use('/api/trust', trustRoute);
 
 
 app.listen(port,()=>{
